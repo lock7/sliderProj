0d785512e1f8fdde4f5696c507ff2952dda7027e
Kathryn Burgess <info@kathrynburgess.com>
2019-04-04 10:25:42 +0100

    first push from Kathryn

---
+++ card-slider-task/src/Components/App/index.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import data from "../../../data/db.json";
+import Card from "../Card";
+import Display from "../Display";
+
+class App extends Component {
+  render() {
+    return <div>Card Slider Demo</div>;
+  }
+}
+
+export default App;
---
+++ card-slider-task/src/Components/Card/index.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+class Card extends Component {
+  render() {
+    return <div />;
+  }
+}
+
+export default Card;
+//build frame of card
+//import data on to card
+//display then processes the card
